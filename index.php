92ae0a188dc04b437ea7805b2fe79b0d820c1c0b
Nabilatul Badiati <nabilabadiati@gmail.com>
2022-10-24 11:02:52 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama ="Nabila";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik ="Atilla";
+$umurAdik = 18;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
